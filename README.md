e4c892b41e39951255e0972d9fafbd7a428140d4
Choens <andy.choens@gmail.com>
2024-02-22 22:17:54 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+Midterm exam for Intro to Data.
---
+++ dependencies.R
@@ -0,0 +1,15 @@
+## Simple script used to install all packages needed by the lecture/lab.
+
+p <- c(
+  "knitr",
+  "janitor",
+  "markdown",
+  "rio",
+  "rmarkdown",
+  "shiny",
+  "tidyverse",
+  "Stat2Data"
+)
+install.packages(p)
+
+unlink("quiz-answers.qmd")
